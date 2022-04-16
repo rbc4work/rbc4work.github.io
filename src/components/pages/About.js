4379e74eaee09faf883f7eb1c8291cccdb9bd2fb
rbc4work <91816942+rbc4work@users.noreply.github.com>
2022-04-16 15:56:45 +0530

    Update with contact us

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,34 +12,41 @@ export default function About() {
             <h1>About Us</h1>
             <br />
             <br />
-            <h2>
-              Pixel Perfect is a full service photo-editing and retouching
-              company specializing in Real Estate services, Portrait retouching,
-              360 degree Panorama photo editing.
-            </h2>
-            <br />
-            <h2>
-              We are a company powered by experienced talented and passionate
-              team that focus on serving our clients with their utmost
-              satisfaction.
-            </h2>
-            <h2>
-              Our innovation, creation, deduction, and teamwork have helped us
-              delve deep into the work process to find out the best solutions
-              for our clients.
-            </h2>
+            <div style={{ margin: "auto", width: "80%" }}>
+              <h2>
+                Pixel Perfect is a full service photo-editing and retouching
+                company specializing in Real Estate services, Portrait
+                retouching, 360 degree Panorama photo editing.
+              </h2>
+              <br />
+              <h2>
+                We are a company powered by experienced talented and passionate
+                team that focus on serving our clients with their utmost
+                satisfaction.
+              </h2>
+              <h2>
+                Our innovation, creation, deduction, and teamwork have helped us
+                delve deep into the work process to find out the best solutions
+                for our clients.
+              </h2>
+            </div>
           </li>
         </ul>
         <ul className="cards__items_pricing">
           <li className="cards__item_text">
-            <h1> Contact Us </h1>
-            <br />
-            <h2>Address:</h2>
-            <br />
-            <h3>Pixel Perfect</h3>
-            <h3> Moghalrajapuram, Vijayawada 520010</h3>
-            <br />
-            <h3>Contact Number: +91 8555034492 / +91 9704442400</h3>
+            <div style={{ margin: "auto", width: "80%" }}>
+              <h1 style={{ textAlign: "left" }}> Contact Us </h1>
+              <br />
+              <h2>Address:</h2>
+              <br />
+              <h3>Pixel Perfect</h3>
+              <h3> Moghalrajapuram, Vijayawada 520010</h3>
+              <br />
+              <h2>Contact Number:</h2>
+              <br />
+              <h3> +91 8555034492</h3>
+              <h3> +91 9704442400</h3>
+            </div>
           </li>
         </ul>
       </div>
